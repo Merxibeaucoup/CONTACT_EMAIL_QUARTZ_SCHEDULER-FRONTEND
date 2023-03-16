b02ea8aafdf00f54c2032c81e83a0777c4a55951
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-03-15 22:41:30 -0400

    update and delete started

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/auth/AuthContext";
 import newRequest from "../utils/newRequest";
 
@@ -13,8 +12,6 @@ const Modal = ({ setModal }) => {
   });
 
   const { user } = useContext(AuthContext);
-
-  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setContact((prev) => {
